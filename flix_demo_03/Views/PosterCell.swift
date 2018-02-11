6d150710426731cb48e900f528d320d98dbc974d
Carlos Huizar-Valenzuela <carlos_huizar@Carloss-MacBook-Pro.local>
2018-02-11 12:56:55 -0800

    FInished Week 2 Assignment

---
+++ flix_demo_03/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_demo_03
+//
+//  Created by Carlos Huizar-Valenzuela on 2/7/18.
+//  Copyright © 2018 Carlos Huizar-Valenzuela. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
